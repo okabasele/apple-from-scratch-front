e6183323fd162d3454ff646a7729a29d325ce0a3
okabasele <ocekabasele@gmail.com>
2024-06-17 13:34:05 +0200

    feat:  update banner & product grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,56 +23,51 @@ export default async function Home() {
       subtitle: "Titane. Si robuste. Si léger. Si Pro.",
       image: "/images/banners/iphone_15_pro.jpg",
       color:"white",
-      item: products.find(product=> product.name == "iPhone 15 Pro")
+      item: products.filter(product=> product.name === "iPhone 15 Pro")[0]
     },
     {
       title: "iPhone 15",
       subtitle: "Nouvel appareil photo. Nouvelles couleurs. Réemerveillement.",
       image: "/images/banners/iphone_15.jpg",
       color:"black",
-      item: products.find(product=> product.name == "iPhone 15")
+      item: products.filter(product=> product.name === "iPhone 15")[0]
   
     }
   ]
 
   const grid = [
- {
-  
-    title: "MacBook Air",
-    subtitle: "Poids plume. Puissance M3.",
-    image: "/images/promo/macbook_air_m3.jpg",
-    url: "#",
-    item: products.find(product=> product.name == "Macbook Air M3")
-  },
-  {
-    
-    title: "Apple Watch",
-    subtitle: "Clairement brillante",
-    text:"Series 9",
-    className:"text-red-500",
-    image: "/images/promo/apple_watch_series_9.jpg",
-    url: "#",
-    item: products.find(product=> product.name == "Apple Watch")
- },
- {
-  
-  title: "iPad",
-  subtitle:"Vos idées. Sa magie. Du génie.",
-  image: "/images/promo/ipads.jpg",
-  url: "#",
-  item: products.find(product=> product.name == "iPad")
-  },
-  {
-    
-    title: "AirPods Pro",
-    subtitle: "Nouveau son sur mesure.",
-    text: "Audio adaptatif.",
-    className:"text-red-500",
-    image: "/images/promo/airpods_pro.jpg",
-    url: "#",
-    item: products.find(product=> product.name == "AirPods Pro")
- }
-
+    {
+      title: "MacBook Air",
+      subtitle: "Poids plume. Puissance M3.",
+      image: "/images/promo/macbook_air_m3.jpg",
+      url: "#",
+      item: products.filter(product=> product.name === "Macbook Air M3")[0]
+    },
+    {
+      title: "Apple Watch",
+      subtitle: "Clairement brillante",
+      text:"Series 9",
+      className:"text-red-500",
+      image: "/images/promo/apple_watch_series_9.jpg",
+      url: "#",
+      item: products.filter(product=> product.name === "Apple Watch")[0]
+    },
+    {
+      title: "iPad",
+      subtitle:"Vos idées. Sa magie. Du génie.",
+      image: "/images/promo/ipads.jpg",
+      url: "#",
+      item: products.filter(product=> product.name === "iPad")[0]
+    },
+    {
+      title: "AirPods Pro",
+      subtitle: "Nouveau son sur mesure.",
+      text: "Audio adaptatif.",
+      className:"text-red-500",
+      image: "/images/promo/airpods_pro.jpg",
+      url: "#",
+      item: products.filter(product=> product.name === "AirPods Pro")[0]
+    }
   ]
 
 
